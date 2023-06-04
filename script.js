69adabea520382d4d38d6ab7f12b3193a07b7b7e
namazu <n+gh@ynr.jp>
2023-06-04 23:53:22 +0900

    画像の表示設定変更

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var date = new Date();
 var month = date.getMonth();
 
-if (month >= 3 && month <= 5) {
+if (month >= 2 && month <= 4) {
     document.body.classList.add('spring');
-} else if (month >= 6 && month <= 8) {
+} else if (month >= 5 && month <= 7) {
     document.body.classList.add('summer');
-} else if (month >= 9 && month <= 11) {
+} else if (month >= 8 && month <= 10) {
     document.body.classList.add('autumn');
 } else {
     document.body.classList.add('winter');
